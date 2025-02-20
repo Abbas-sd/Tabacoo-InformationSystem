f0d529b90c3b6bb6db6d7138f8813ae7c5af1686
Hadi208 <hadikhalaf007@gmail.com>
2025-02-20 11:29:51 +0200

    haaaadi

--- TobaccoStore/TobaccoStore/Customer Order With barcode.Designer.cs
+++ TobaccoStore/TobaccoStore/Customer Order With barcode.Designer.cs
@@ -69,7 +69,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(484, 508);
+            this.button1.Location = new System.Drawing.Point(552, 512);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
